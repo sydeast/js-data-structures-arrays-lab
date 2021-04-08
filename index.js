58b9e7a20e68bc2404ff97e791717b20dced830c
Syd <24235464+sydeast@users.noreply.github.com>
2021-04-07 19:07:36 -0500

    commit tree 6af47d66ee59871d76dc0aac906c591f4e8a3106

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 // Write your solution here!
+let drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver(name) {
+    drivers.push(name);
+};
+
+function destructivelyPrependDriver(name) {
+    drivers.unshift(name);
+};
+
+function destructivelyRemoveLastDriver() {
+    drivers.pop();
+};
+
+function destructivelyRemoveFirstDriver() {
+    drivers.shift();
+};
+
+function appendDriver (name) {
+  let newDrivers = drivers.slice()
+  newDrivers.push(name);
+  return newDrivers;
+};
+// Another Way:
+// function appendDriver(name){
+//   let newDriver = [...drivers, name]
+//   return newDriver;
+// }
+
+function prependDriver (name) {
+  let newDrivers = drivers.slice()
+  newDrivers.unshift(name);
+  return newDrivers;
+};
+// function prependDriver(name){
+//   let newDriver = [name, ...drivers]
+//   return newDriver;
+// }
+
+function removeLastDriver() {
+  let removeDrivers = drivers.slice()
+  removeDrivers.pop();
+  return removeDrivers;
+};
+// return newDrivers.slice(0, newDrivers.length - 1);
+
+function removeFirstDriver() {
+    let removeFirst = drivers.slice(1);
+    return removeFirst;
+};
